630aa07939a78df3263a3e7113060e40d674730d
Franco Stramana <franco.stramana@gmail.com>
2020-09-14 20:15:07 -0300

    fix tbody

--- peliculas.php
+++ peliculas.php
@@ -16,14 +16,15 @@ if (!isset($_GET['genre']) || empty($_GET['genre'])) {
 // obtiene el genero enviado por GET 
 $genre = $_GET['genre'];
 
-echo "<h1>Lista por género: $genre</h2>";
-echo "<a href='index.html'> Volver </a>";
-
 // obtiene la lista de peliculas de la DB según género
 $db = new PDO('mysql:host=localhost;'.'dbname=db_movies;charset=utf8', 'root', '');
 $query = $db->prepare('SELECT * FROM movies WHERE genre = ?');
 $query->execute([$genre]);
 $movies = $query->fetchAll(PDO::FETCH_OBJ);
+
+
+echo "<h1>Lista por género: $genre</h2>";
+echo "<a href='index.html'> Volver </a>";
 
 // imprime la tabla de peliculas
 echo "<table>
@@ -34,16 +35,16 @@ echo "<table>
                 <th>Estudio</th>
             </tr>
         <thead>
+        <tbody>
 ";
 foreach($movies as $movie) {
     echo "
-        <tbody>
             <tr>
                 <td>$movie->title</td>
                 <td>$movie->year</td>
                 <td>$movie->studio</td>
             </tr>
-        </tbody>    
     ";
 }
-echo "</table>";
+echo " </tbody>    
+    </table>";
